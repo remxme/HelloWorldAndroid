726132a2d84e0c4311b92080af16c878cac53d89
daimon <remxme@gmail.com>
2014-07-04 21:51:22 +0800

    1

--- HelloWorld/gen/com/example/helloworld/R.java
+++ HelloWorld/gen/com/example/helloworld/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000f;
         public static final int age=0x7f080005;
         public static final int button1=0x7f080002;
+        public static final int gotourl=0x7f08000d;
         public static final int listview=0x7f080003;
         public static final int myButton=0x7f080001;
         public static final int myTextView=0x7f080000;
@@ -37,6 +38,8 @@ public final class R {
         public static final int textView1=0x7f080007;
         public static final int textView2=0x7f08000a;
         public static final int username=0x7f080006;
+        public static final int webview=0x7f08000e;
+        public static final int webviewurl=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,6 +47,7 @@ public final class R {
         public static final int listview_row=0x7f030002;
         public static final int login=0x7f030003;
         public static final int test=0x7f030004;
+        public static final int web_browse=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
